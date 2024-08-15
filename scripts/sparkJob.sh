a8c72d0b50906598b9fb4472287a935cb1733ea0
Simon Peppel <simon.peppel@prodyna.com>
2024-08-15 10:39:02 +0200

    test: temp 3

--- scripts/sparkJob.sh
+++ scripts/sparkJob.sh
@@ -42,7 +42,7 @@ SERVER_URL=$(kind get kubeconfig --name="${CLUSTER_NAME}" | grep 'server:' | awk
 # kubectl apply -f $REPO_ROOT_DIR/k8s/spark/base/role.yaml
 # kubectl apply -f $REPO_ROOT_DIR/k8s/spark/base/role-binding.yaml
 
-kubectl delete pod -n spark spark --force --grace-period=0
+kubectl delete pod -n spark spark2 --force --grace-period=0
 # WIP
 #  --conf spark.kubernetes.container.image=bitnami/spark-with-user:latest \
 spark-submit \
@@ -53,8 +53,8 @@ spark-submit \
  --deploy-mode cluster \
  --conf spark.kubernetes.namespace=spark \
  --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
- --conf spark.kubernetes.driver.pod.name=spark \
- --conf spark.kubernetes.executor.podNamePrefix=spark-executor \
+ --conf spark.kubernetes.driver.pod.name=spark2 \
+ --conf spark.kubernetes.executor.podNamePrefix=spark-executor2 \
  --conf spark.kubernetes.driver.podTemplateContainerName=spark \
  --conf spark.kubernetes.executor.podTemplateContainerName=spark \
  --conf spark.kubernetes.driverEnv.SPARK_USER=spark \
@@ -63,7 +63,12 @@ spark-submit \
  --conf spark.kubernetes.driver.secretKeyRef.AWS_SECRET_ACCESS_KEY=minio:rootPassword \
  --conf spark.kubernetes.executor.secretKeyRef.AWS_ACCESS_KEY_ID=minio:rootUser \
  --conf spark.kubernetes.executor.secretKeyRef.AWS_SECRET_ACCESS_KEY=minio:rootPassword \
- --conf spark.eventLog.dir='s3a://minio:9000/spark-logs' \
+ --conf spark.eventLog.dir='s3a://spark-logs/spark-logs' \
+ --conf spark.hadoop.fs.s3a.connection.ssl.enabled=false \
+ --conf spark.hadoop.fs.s3a.endpoint=http://minio:9000 \
+ --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
+ --conf spark.hadoop.fs.s3a.path.style.access=true \
+ --conf spark.hadoop.com.amazonaws.services.s3.enableV4=true \
  --num-executors 1 \
  --verbose \
  https://raw.githubusercontent.com/simpe00/test-exmaple-jar/main/pi.py 1
