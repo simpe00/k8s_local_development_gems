0ece38d3ea0a73506e54028758aada4e6cf4e628
Simon Peppel <simon.peppel@prodyna.com>
2024-08-15 08:01:11 +0200

    test: temp 1

--- scripts/installKind.sh
+++ scripts/installKind.sh
@@ -13,7 +13,30 @@ K8S_NAMESPACE=""
 STAGE_ID=local
 
 
+# Extract the cluster name from the kind-no-ssl.yaml file
+CLUSTER_NAME=$(grep 'name:' ${REPO_ROOT_DIR}/kind/kind-no-ssl.yaml | awk '{print $2}')
 
+# Check if the cluster exists
+if kind get clusters | grep -q "$CLUSTER_NAME"; then
+    echo "Cluster $CLUSTER_NAME exists."
+    read -p "Do you want to restart the cluster? (yes/no): " user_input
+
+    # Check the user's input
+    if [[ "$user_input" == "yes" ]]; then
+        echo "Restarting the cluster."
+        kind delete cluster --name ${CLUSTER_NAME}
+        kind create cluster --config ${GITLAB_REPO_ROOT_DIR}/kind/kind-no-ssl.yaml
+    elif [[ "$user_input" == "no" ]]; then
+        echo "Use the existing cluster."
+        kubectl config use-context kind-${CLUSTER_NAME}
+    else
+        echo "Invalid input. Please enter 'yes' or 'no'."
+        exit 1
+    fi
+else
+    echo "Cluster $CLUSTER_NAME does not exist."
+    kind create cluster --config ${GITLAB_REPO_ROOT_DIR}/kind/kind-no-ssl.yaml
+fi
 
 kind create cluster --config ${KIND_REPO_ROOT_DIR}/kind/kind-no-ssl.yaml
 
--- scripts/sparkJob.sh
+++ scripts/sparkJob.sh
@@ -6,27 +6,63 @@ SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_ROOT_DIR=$(realpath "$SCRIPT_DIR/../")
 KIND_REPO_ROOT_DIR=$(realpath "$SCRIPT_DIR/../")
 
+CLUSTER_NAME=$(grep 'name:' ${REPO_ROOT_DIR}/kind/kind-no-ssl.yaml | awk '{print $2}')
+SERVER_URL=$(kind get kubeconfig --name="${CLUSTER_NAME}" | grep 'server:' | awk '{print $2}' )
+
 # WIP
 # https://spark.apache.org/docs/latest/running-on-kubernetes
 #spark-submit \
 #  --class org.apache.spark.examples.SparkPi \
 #  --conf spark.kubernetes.container.image=bitnami/spark:latest \
-#  --master k8s://https://127.0.0.1:44255 \
+#  --master k8s://https://127.0.0.1:45521 \
 #  --conf spark.kubernetes.driverEnv.SPARK_MASTER_URL=spark://spark-master-0.spark-headless.spark.svc.cluster.local:7077 \
 #  --deploy-mode cluster \
 #  --conf spark.kubernetes.namespace=spark \
 #   https://github.com/simpe00/test-exmaple-jar/raw/main/spark-examples_2.12-3.5.1.jar 1
 
+# http://minio-console-127-0-0-1.nip.io:32180/login
+helm repo add minio https://charts.min.io/
+helm repo update minio
+helm upgrade \
+	--install \
+	minio \
+	minio/minio \
+	--namespace spark \
+	--create-namespace \
+	-f $REPO_ROOT_DIR/k8s/minio/base/values.yaml
+
+
+
+docker build . -f $REPO_ROOT_DIR/docker/spark/Dockerfile -t bitnami/spark-with-user:latest
+kind load docker-image bitnami/spark-with-user:latest -n pilatus-k8s
+
+
+
+kubectl apply -f $REPO_ROOT_DIR/k8s/spark/base/sa.yaml
+kubectl apply -f $REPO_ROOT_DIR/k8s/spark/base/role.yaml
+kubectl apply -f $REPO_ROOT_DIR/k8s/spark/base/role-binding.yaml
+
+kubectl delete pod -n spark spark --force --grace-period=0
 # WIP
-#spark-submit \
-#  --conf spark.kubernetes.container.image=bitnami/spark:latest \
-#  --master k8s://https://127.0.0.1:44255 \
-#  --conf spark.kubernetes.driverEnv.SPARK_MASTER_URL=spark://spark-master-0.spark-headless.spark.svc.cluster.local:7077 \
-#  --deploy-mode cluster \
-#  --conf spark.kubernetes.namespace=spark \
-#  --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
-#  --verbose \
-#  https://raw.githubusercontent.com/simpe00/test-exmaple-jar/main/pi.py 1
+#  --conf spark.kubernetes.container.image=bitnami/spark-with-user:latest \
+spark-submit \
+ --class org.apache.spark.examples.SparkPi \
+ --conf spark.kubernetes.container.image=bitnami/spark-with-user:latest \
+ --conf spark.kubernetes.executor.container.image=bitnami/spark-with-user:latest \
+ --master k8s://${SERVER_URL} \
+ --deploy-mode cluster \
+ --conf spark.kubernetes.namespace=spark \
+ --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+ --conf spark.kubernetes.driver.pod.name=spark \
+ --conf spark.kubernetes.executor.podNamePrefix=spark-executor \
+ --conf spark.kubernetes.driver.podTemplateContainerName=spark \
+ --conf spark.kubernetes.executor.podTemplateContainerName=spark \
+ --conf spark.kubernetes.driverEnv.SPARK_USER=spark \
+ --conf spark.eventLog.enabled=false \
+ --conf spark.eventLog.dir='s3a://my-bucket/spark-logs' \
+ --num-executors 1 \
+ --verbose \
+ https://raw.githubusercontent.com/simpe00/test-exmaple-jar/main/pi.py 1
 
 
 # Test submit
@@ -36,6 +72,6 @@ KIND_REPO_ROOT_DIR=$(realpath "$SCRIPT_DIR/../")
 #	https://raw.githubusercontent.com/simpe00/test-exmaple-jar/main/pi.py 1
 
 # Example 2
-kubectl exec -ti --namespace spark spark-master-0 -- spark-submit --master spark://spark-master-svc:7077 \
-	--class org.apache.spark.examples.SparkPi \
-	https://github.com/simpe00/test-exmaple-jar/raw/main/spark-examples_2.12-3.5.1.jar 1
+# kubectl exec -ti --namespace spark spark-master-0 -- spark-submit --master spark://spark-master-svc:7077 \
+# 	--class org.apache.spark.examples.SparkPi \
+# 	https://github.com/simpe00/test-exmaple-jar/raw/main/spark-examples_2.12-3.5.1.jar 1
